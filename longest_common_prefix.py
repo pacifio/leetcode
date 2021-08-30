2426196bb58cffe7fbe73a62de27f9fee9be91d3
Adib Mohsin <adibmohsin.root@gmail.com>
2021-08-31 00:19:56 +0600

    longest_common_prefix

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,7 +1,22 @@
 class Solution:
     def longestCommonPrefix(self, strs) -> str:
-        pass
+        x = [set(i) for i in strs]
+        r = ''.join([x[i-1].intersection(x[i])
+                     for i in range(1, len(x))][-1])[::-1]
+        c = False
+        if len(r) > 0:
+            for i in strs:
+                if i.startswith(r):
+                    c = True
+                else:
+                    c = False
+            if c:
+                return r
+            else:
+                return r[::-1]
+        else:
+            return ''
 
 
 s = Solution()
-print(s.longestCommonPrefix(['dick', 'duck', 'floof']))
+print(s.longestCommonPrefix(["flower","flow","flight"]))
